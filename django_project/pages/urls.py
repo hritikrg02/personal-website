184d0fc74ef3f2742f9d73ea58452a59834c7163
Ricky Saynganthone <hritikrg02@gmail.com>
2026-06-16 14:58:18 -0400

    black

--- django_project/pages/urls.py
+++ django_project/pages/urls.py
@@ -4,10 +4,6 @@ from .views import home_page_view, about_page_view
 
 # the route is the subdirectory based on what's in the django-project/urls.py
 urlpatterns = [
-    path(
-        "", home_page_view, name="home"
-    ),
-    path(
-        "about/", about_page_view, name="about"
-    )
+    path("", home_page_view, name="home"),
+    path("about/", about_page_view, name="about"),
 ]
--- django_project/pages/views.py
+++ django_project/pages/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home_page_view(request):  # function-based view
     return render(request, "home.html")
 
+
 def about_page_view(request):
-    cxt = {"name": "Ricky", "age": "24"}  # this is usually loaded via the model and gotten through the database
+    cxt = {
+        "name": "Ricky",
+        "age": "24",
+    }  # this is usually loaded via the model and gotten through the database
     return render(request, "about.html", cxt)
