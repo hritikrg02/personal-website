bc13cfc757fcd333da6c320c3358c0a6c864f877
Ricky Saynganthone <hritikrg02@gmail.com>
2026-06-17 13:01:11 -0400

    deleted about page

--- django_project/pages/urls.py
+++ django_project/pages/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import home_page_view, about_page_view
+from .views import home_page_view
 
 # the route is the subdirectory based on what's in the django-project/urls.py
 urlpatterns = [
     path("", home_page_view, name="home"),
-    path("about/", about_page_view, name="about"),
 ]
--- django_project/pages/views.py
+++ django_project/pages/views.py
@@ -7,5 +7,3 @@ def home_page_view(request):  # function-based view
     return render(request, "home.html")
 
 
-def about_page_view(request):
-    return render(request, "about.html")
