022685429e81531d9cd39f2a4837840c6dad5596
Ricky Saynganthone <hritikrg02@gmail.com>
2026-06-17 02:17:32 -0400

    updated home to have more items in bar

--- django_project/pages/views.py
+++ django_project/pages/views.py
@@ -8,8 +8,4 @@ def home_page_view(request):  # function-based view
 
 
 def about_page_view(request):
-    cxt = {
-        "name": "Ricky",
-        "age": "24",
-    }  # this is usually loaded via the model and gotten through the database
-    return render(request, "about.html", cxt)
+    return render(request, "about.html")
